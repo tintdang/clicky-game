2d002d2c0e4d1b2924045b424160af6540f5a211
TinTDang <tin2dang@gmail.com>
2018-09-20 17:26:39 -0500

    Added Game component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Wrapper from './components/Wrapper'
 import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
+import Game from './components/Game'
 
 const App = () => (
   <Wrapper>
     <Navbar />
     <Jumbotron />
+    <Game />
   </Wrapper>
 );
 
---
+++ src/components/Game/Game.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Game = () => 
+<div className="container">
+
+</div>;
+
+
+export default Game;
