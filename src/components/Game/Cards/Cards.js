3971fa3111bff3eef7a83f5a53faedfd5ebff9bc
TinTDang <tin2dang@gmail.com>
2018-09-20 18:24:02 -0500

    added card component inside game component and added flexrows

---
+++ src/components/Game/Cards/Cards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Cards.css';
+
+const Card = () =>
+<div className="card">
+    <h1>This is a card</h1>
+</div>
+
+export default Card;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import "./Game.css";
+import Card from './Cards';
 
-const Game = () => 
-<div className="container">
+const Game = () =>
+    <div className="container">
+        <div className="d-flex justify-content-center cardrow">
 
-</div>;
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+
+        <div className="d-flex justify-content-center cardrow">
+
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+
+        <div className="d-flex justify-content-center cardrow">
+
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+    </div>;
 
 
 export default Game;
