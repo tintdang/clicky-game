f391a0e4685c4b55dbcf91a19bb8bfd243004d67
TinTDang <tin2dang@gmail.com>
2018-09-20 17:16:44 -0500

    updated navbar css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () =>
 <nav className="navbar">
     <a className= "navbar-brand" href="#">Clicky Game</a>
     <span className="navbar-text">Click an image to begin!</span>
+    <span>Score: 0 | Top Score: 0</span>
 </nav>;
 
 export default Navbar;
