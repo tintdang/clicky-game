c8393293ef04e072b915bcaa7e427ce902081459
TinTDang <tin2dang@gmail.com>
2018-09-20 16:44:51 -0500

    Added Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => 
+<nav className="navbar">
+    <a href="#">Clicky Game</a>
+    <span className="navbar-brand"></span>
+</nav>;
+
+export default Navbar;
