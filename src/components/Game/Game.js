54c64054dc51a47b5eeb410aace53b674e18c6a8
TinTDang <tin2dang@gmail.com>
2018-09-21 21:51:05 -0500

    created shuffle function

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,6 +13,19 @@ class Game extends React.Component {
 
   componentDidMount() {
     console.log("I mounted!")
+    console.log(`The json is: \n${JSON.stringify(game)}`)
+    console.log(`I want to shuffle this json file`)
+    this.shuffle(game)
+    console.log(`Shuffled! ${JSON.stringify(game)}`)
+  }
+
+  //This will shuffle the cards and return a mixed array
+  shuffle = game => {
+    for (let i = game.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [game[i], game[j]] = [game[j], game[i]];
+    }
+    return this.setState({ game })
   }
 
   handleClick = id => {
@@ -23,18 +36,20 @@ class Game extends React.Component {
 
     // checks if the clicked status is true, if true: alert me.
     // Clear the score back to 0 and reset the isClicked states back to false
-    if (this.state.game[id - 1].isClicked){
+    // then shuffle the game
+    if (this.state.game[id - 1].isClicked) {
       alert("YOU CLICKED ME ALREADY")
       const game = this.state.game.map(card => {
         card.isClicked = false
         return card
       })
-      return this.setState( {count: 0, game });
+      this.shuffle(game)
+      return this.setState({ count: 0, game });
     }
-    
+
     //makes new function where it looks for the specific id and updates the state to be true, this still needs to render the rest of the cards
     const game = this.state.game.map(card => {
-      if (card.id === id){
+      if (card.id === id) {
         console.log(`This is the right id cause ${card.id} = ${id}`)
         card.isClicked = true
         return card
@@ -42,7 +57,11 @@ class Game extends React.Component {
         return card
       }
     })
+
+    // once the update is done, shuffle the array again and have them guess MUAHAHAHAHAHHAHA
+
     console.log(game)
+    this.shuffle(game)
     // This updates the counter if a card is clicked
     // and will update the game's current state
     this.setState({ count: this.state.count + 1, game });
