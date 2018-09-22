2cb2f91671852a97f7ba82d6fb7e941f87bf83d1
TinTDang <tin2dang@gmail.com>
2018-09-21 23:29:45 -0500

    updated jumbotron message

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -4,6 +4,7 @@ import './Jumbotron.css'
 const Jumbotron = () => 
 <div className="jumbotron">
     <h1>This is a clicky game</h1>
+    <p>Click on a picture to score a point, but don't click on the same picture twice!</p>
 </div>
 
 export default Jumbotron;
