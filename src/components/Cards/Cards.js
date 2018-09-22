8fd29e3edd75307a84eb173d05100ba8ff196a08
TinTDang <tin2dang@gmail.com>
2018-09-21 21:25:19 -0500

    Got cards to change their states

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Cards.css';
 
 const Card = props => 
-  <div className="card" onClick={() => props.handleClick()}>
+  <div className="card" onClick={() => props.handleClick(props.id)}>
     <div>
-      <img className="img-fluid" src={props.image} alt="something" />
+      <img className="img-fluid" src={props.image} alt={props.id} />
     </div>
   </div>;
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,37 +3,49 @@ import "./Game.css";
 import Card from '../Cards';
 import Navbar from '../Navbar';
 import Jumbotron from '../Jumbotron';
+import game from "../../cards.json"
 
 class Game extends React.Component {
   state = {
     count: 0,
-    game: [
-      {
-        id: 1,
-        img: "https://gbf.wiki/images/1/1a/Freezie_B.png",
-        isClicked: false
-      },
-      {
-        id: 2,
-        img: "https://gbf.wiki/images/b/b9/Freezie_A.png",
-        isClicked: false
-      },
-      {
-        id: 3,
-        img: "https://gbf.wiki/images/thumb/3/3e/Scathacha_NPC.png/480px-Scathacha_NPC.png",
-        isClicked: false
-      },
-    ],
+    game
   }
 
   componentDidMount() {
     console.log("I mounted!")
   }
 
-  handleClick = () => {
+  handleClick = id => {
+    //This states what card is selected when pressed
+    console.log(`You clicked on id: ${this.state.game[id - 1].id}`)
+    console.log(`the current index is ${id - 1}`)
+    console.log(`The current state of this card's clicked status is ${this.state.game[id - 1].isClicked}`)
+
+    // checks if the clicked status is true, if true: alert me.
+    // Clear the score back to 0 and reset the isClicked states back to false
+    if (this.state.game[id - 1].isClicked){
+      alert("YOU CLICKED ME ALREADY")
+      const game = this.state.game.map(card => {
+        card.isClicked = false
+        return card
+      })
+      return this.setState( {count: 0, game });
+    }
+    
+    //makes new function where it looks for the specific id and updates the state to be true, this still needs to render the rest of the cards
+    const game = this.state.game.map(card => {
+      if (card.id === id){
+        console.log(`This is the right id cause ${card.id} = ${id}`)
+        card.isClicked = true
+        return card
+      } else {
+        return card
+      }
+    })
+    console.log(game)
     // This updates the counter if a card is clicked
-    this.setState({ count: this.state.count + 1 });
-    console.log(this.state.count + 1)
+    // and will update the game's current state
+    this.setState({ count: this.state.count + 1, game });
   }
 
   render() {
@@ -50,6 +62,7 @@ class Game extends React.Component {
 
           {this.state.game.map(card => (
             <Card
+              id={card.id}
               key={card.id}
               handleClick={this.handleClick}
               image={card.img}
