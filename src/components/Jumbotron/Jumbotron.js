8431ffc7f9fdc5fda61e700d3b981ce839a66596
TinTDang <tin2dang@gmail.com>
2018-09-22 00:01:17 -0500

    added jumbotron background

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Jumbotron.css'
 
-const Jumbotron = () => 
-<div className="jumbotron">
-    <h1>This is a clicky game</h1>
-    <p>Click on a picture to score a point, but don't click on the same picture twice!</p>
-</div>
+const Jumbotron = () =>
+    <div className="jumbotron">
+        <div className="bg">
+        </div>
+        <div className="content">
+            <h1>This is a clicky game</h1>
+            <p>Click on a picture to score a point, but don't click on the same picture twice!</p>
+        </div>
+    </div>
 
 export default Jumbotron;
